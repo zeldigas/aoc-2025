dfa93f690a4331e9b9f1b29abc2d910c22f163e7
Dmitry Pavlov <zeldigas@gmail.com>
2025-12-01 09:19:47 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,68 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var zeroCounter = 0
+        var arrow = 50
+        input.forEach { step ->
+            val direction = step[0]
+            val amount = step.substring(1).toInt() % 100
+            when (direction) {
+                'L' -> {
+                    arrow -= amount
+                    if (arrow < 0) {
+                        arrow += 100
+                    }
+                }
+                'R' -> {
+                    arrow += amount
+                    arrow %= 100
+                }
+            }
+            if (arrow == 0) {
+                zeroCounter++
+            }
+        }
+        return zeroCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var zeroCounter = 0
+        var arrow = 50
+        input.forEach { step ->
+            val direction = step[0]
+            val totalTurns = step.substring(1).toInt()
+            val full360Turns = totalTurns / 100
+            zeroCounter += full360Turns // each 360 turn will cross 0
+            val amount = totalTurns % 100
+            when (direction) {
+                'L' -> {
+                    val prevValue = arrow
+                    arrow -= amount
+                    if (arrow < 0) {
+                        arrow += 100
+                        if (prevValue != 0) {
+                            zeroCounter++
+                        }
+                    }
+                }
+                'R' -> {
+                    arrow += amount
+                    if (arrow > 100) {
+                        zeroCounter++
+                    }
+                    arrow %= 100
+                }
+            }
+            if (arrow == 0) {
+                zeroCounter++
+            }
+        }
+        return zeroCounter
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
